a0275def79a559b8ba39fb865ef735c236e915dd
Hüseyn Alıyev <huseynma@code.edu.az>
2023-03-29 20:27:43 +0400

    Added Controller For DiscountApi

---
+++ Services/Discount/Nest.Services.DiscountApi/Controllers/DiscountsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nest.Services.DiscountApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+    }
+}
